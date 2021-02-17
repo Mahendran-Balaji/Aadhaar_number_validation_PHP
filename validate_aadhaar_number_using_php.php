868a39c55f24d9389f0266c25e26e6a57f4831ac
Mahendran <39937781+Mahendran-Balaji@users.noreply.github.com>
2021-02-17 17:51:12 +0530

    Create validate_aadhaar_number_using_php.php

---
+++ validate_aadhaar_number_using_php.php
@@ -0,0 +1,57 @@
+<?php
+class AadhaarValidateHelper
+{
+    public static function isAadharValid($num,$permutation,$dihedral) {
+        settype($num, "string");
+        $expectedDigit = substr($num, -1);
+        $actualDigit = $this->CheckSumAadharDigit(substr($num, 0, -1),$permutation,$dihedral);
+        return ($expectedDigit == $actualDigit) ? $expectedDigit == $actualDigit : 0;
+    }
+
+    public static function CheckSumAadharDigit($partial,$permutation,$dihedral) {
+        $inverse = array(0, 4, 3, 2, 1, 5, 6, 7, 8, 9);
+        settype($partial, "string");
+        $partial = strrev($partial);
+        $digitIndex = 0;
+        for ($i = 0; $i < strlen($partial); $i++) {
+            $digitIndex = $dihedral[$digitIndex][$permutation[($i + 1) % 8][$partial[$i]]];
+        }
+        return $inverse[$digitIndex];
+    }
+    
+    public static function aadharcardValidation($AadharNo) {
+        $dihedral = array(
+            array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+            array(1, 2, 3, 4, 0, 6, 7, 8, 9, 5),
+            array(2, 3, 4, 0, 1, 7, 8, 9, 5, 6),
+            array(3, 4, 0, 1, 2, 8, 9, 5, 6, 7),
+            array(4, 0, 1, 2, 3, 9, 5, 6, 7, 8),
+            array(5, 9, 8, 7, 6, 0, 4, 3, 2, 1),
+            array(6, 5, 9, 8, 7, 1, 0, 4, 3, 2),
+            array(7, 6, 5, 9, 8, 2, 1, 0, 4, 3),
+            array(8, 7, 6, 5, 9, 3, 2, 1, 0, 4),
+            array(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+        );
+        $permutation = array(
+            array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+            array(1, 5, 7, 6, 2, 8, 3, 0, 9, 4),
+            array(5, 8, 0, 3, 7, 9, 6, 1, 4, 2),
+            array(8, 9, 1, 6, 0, 4, 3, 5, 2, 7),
+            array(9, 4, 5, 3, 1, 2, 6, 8, 7, 0),
+            array(4, 2, 8, 6, 5, 7, 3, 9, 0, 1),
+            array(2, 7, 9, 3, 8, 0, 6, 4, 1, 5),
+            array(7, 0, 4, 6, 9, 1, 3, 2, 5, 8)
+        );
+        
+        if(is_numeric($AadharNo)) {
+            $valid = $this->isAadharValid($AadharNo,$permutation,$dihedral);
+            if ($valid == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+           return false;
+        }
+        
+    }
